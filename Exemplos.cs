30b2c76f1085d73262d6ac5f06c2041345f5767f
Cleberson Costa <cleberson-costa@outlook.com>
2025-08-07 15:56:12 -0300

    init: first commit

---
+++ Exemplos.cs
@@ -0,0 +1,103 @@
+﻿namespace AlunoNota;
+
+enum Direcao
+{
+    Norte,
+    Sul,
+    Leste,
+    Oeste
+}
+
+class Exemplos
+{
+    public static void Direcao(Direcao direcao)
+    {
+        switch (direcao)
+        {
+            case AlunoNota.Direcao.Norte:
+                Console.WriteLine("Você está indo para cima.");
+                break;
+            case AlunoNota.Direcao.Sul:
+                Console.WriteLine("Você está indo para baixo.");
+                break;
+            case AlunoNota.Direcao.Leste:
+                Console.WriteLine("Você está indo para direita.");
+                break;
+            case AlunoNota.Direcao.Oeste:
+                Console.WriteLine("Você está indo para esquerda.");
+                break;
+            default:
+                Console.WriteLine("Direção inválida.");
+                break;
+        }
+    }
+
+    public void Principal()
+    {
+        //Program.Direcao(AlunoNota.Direcao.Norte);
+
+        string[] Turma = new string[0];
+        Turma = new string[] { "João", "Maria", "Pedro", "Ana" };
+
+        List<string> TurmaEmLista = Turma.ToList();
+        TurmaEmLista.Insert(0, "Lucas");
+        TurmaEmLista.RemoveAt(TurmaEmLista.Count - 1);
+
+        foreach (string Aluno in TurmaEmLista)
+        {
+            Console.WriteLine(Aluno);
+        }
+
+        int[] Notas = new[] { 5, 7, 9, 10 };
+        int NumeroDeNotas = Notas.Length;
+
+        for (int i = 0; i < NumeroDeNotas; i++)
+        {
+            Console.WriteLine($"Nota {i + 1}: {Notas[i]}");
+        }
+
+        //FIFO
+        Queue<string> Fila = new Queue<string>();
+        Fila.Enqueue("João");
+        Fila.Enqueue("Maria");
+        Fila.Enqueue("Pedro");
+
+        string Nome = Fila.Dequeue(); // Remove o primeiro elemento da fila
+
+        //LIFO
+        Stack<int> Pilha = new Stack<int>();
+        Pilha.Push(1);
+        Pilha.Push(2);
+        Pilha.Push(3);
+
+        int Numero = Pilha.Pop(); // Remove o último elemento adicionado
+
+        Console.Write("Digite o nome do aluno: ");
+        string NomeCompleto = Console.ReadLine();
+
+        Console.Write("Digite a nota do aluno: ");
+        float Nota = float.Parse(Console.ReadLine());
+
+        if (Nota > 0 && Nota < 3)
+        {
+            Console.Write("Nota Baixa!");
+        }
+        else if (Nota >= 3 && Nota < 8)
+        {
+            Console.Write("Nota Média!");
+        }
+        else if (Nota >= 8 && Nota < 9.9)
+        {
+            Console.Write("Nota Alta!");
+        }
+        else if (Nota == 10)
+        {
+            Console.Write("Nota Perfeita!");
+        }
+        else
+        {
+            Console.Write("Nota Inválida!");
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,189 @@
+﻿namespace AlunoNota;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("*********************************************************");
+        Console.WriteLine("*************** Sistema de alunos e notas ***************");
+        Console.WriteLine("*********************************************************");
+
+        List<Aluno> alunos = new();
+        Aluno aluno;
+        do
+        {
+            aluno = new Aluno();
+
+            Console.WriteLine("");
+            Console.Write("Informe o nome do aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            foreach (Disciplinas disciplina in (Disciplinas[])Enum.GetValues(typeof(Disciplinas)))
+            {
+                Console.WriteLine("");
+
+                Console.Write($"Informe a nota do(a) aluno(a) {aluno.Nome} para a disciplina {disciplina}: ");
+                string? notaTemp = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(notaTemp) || !float.TryParse(notaTemp, out float nota) || nota < 0 || nota > 10)
+                {
+                    Console.WriteLine("Nota inválida. Por favor, insira uma nova nota.\n");
+                    Console.Write($"Informe a nota do(a) aluno(a) {aluno.Nome} para a disciplina {disciplina}: ");
+                    notaTemp = Console.ReadLine();
+                }
+
+                aluno.NotaPorDisciplina.Add(disciplina, float.Parse(notaTemp));
+            }
+
+            alunos.Add(aluno);
+
+        } while (ValidarSimNao());
+
+        Console.WriteLine("");
+        Console.WriteLine("*************** REPORT ***************");
+
+        AprovarReprovarAlunos(alunos);
+        CalculoMediaPorAluno(alunos);
+        EscreveListaAlunos(alunos);
+        CalculoMenorCR(alunos);
+    }
+
+    private static bool ValidarSimNao()
+    {
+        char resposta;
+
+        do
+        {
+            Console.WriteLine("");
+            Console.Write("Deseja cadastrar outro aluno(a)? (S/N) ");
+            char entrada = Console.ReadKey().KeyChar;
+            resposta = Char.ToUpper(entrada);
+
+            if (resposta == 'S')
+            {
+                return true;
+            }
+            else if (resposta == 'N')
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("\nEntrada inválida. Por favor, digite S ou N.");
+            }
+
+        } while (true);
+
+    }
+
+    private static void AprovarReprovarAlunos(List<Aluno> alunos)
+    {
+        foreach (var aluno in alunos)
+        {
+            var itemRecuperado = aluno.NotaPorDisciplina.Sum(i => i.Value) / Enum.GetValues(typeof(Disciplinas)).Length;
+
+            if (itemRecuperado >= 7)
+            {
+                aluno.status = Status.Aprovado;
+            }
+            else
+            {
+                aluno.status = Status.Reprovado;
+            }
+        }
+    }
+    private static void CalculoMediaPorAluno(List<Aluno> alunos)
+    {
+        foreach (var aluno in alunos)
+        {
+            float notas = 0f;
+
+            foreach (KeyValuePair<Disciplinas, float> nota in aluno.NotaPorDisciplina)
+            {
+                notas += nota.Value;
+            }
+
+            aluno.MediaGeral = notas / Enum.GetValues(typeof(Disciplinas)).Length;
+        }
+    }
+    private static void EscreveListaAlunos(List<Aluno> alunos)
+    {
+        Console.WriteLine("");
+        string mensagem = string.Empty;
+
+        foreach (var aluno in alunos)
+        {
+            mensagem = $"Aluno(a): {aluno.Nome} \nMédia Geral: {aluno.MediaGeral.ToString("F2")} \nStatus: {aluno.status}";
+            mensagem += "\nNotas por disciplina: ";
+
+            foreach (KeyValuePair<Disciplinas, float> nota in aluno.NotaPorDisciplina)
+            {
+                mensagem += $"\n{nota.Key}: {nota.Value.ToString("F2")}";
+            }
+
+            Console.WriteLine("\n" + mensagem);
+        }
+    }
+
+    private static void CalculoMenorCR(List<Aluno> alunos)
+    {
+        Aluno? aluno = alunos.MinBy(a => a.MediaGeral);
+
+        if (aluno != null)
+        {
+            Console.WriteLine($"\nO aluno com a menor média é: {aluno.Nome} com média {aluno.MediaGeral.ToString("F2")}");
+        }
+        else
+        {
+            Console.WriteLine("\nNenhum aluno cadastrado.");
+        }
+    }
+}
+
+public class Aluno
+{
+    public string Nome { get; set; }
+
+    public char PrimeiraLetra { get; set; }
+
+    private float _mediaGeral { get; set; }
+
+    public float MediaGeral
+    {
+        get { return _mediaGeral; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("A nota não pode ser negativa.");
+            }
+            _mediaGeral = value;
+        }
+    }
+
+    public Status status;
+
+    public Dictionary<Disciplinas, float> NotaPorDisciplina { get; set; } = new();
+
+    public bool Ativo { get; set; }
+
+    public DateTime Avaliacao { get; set; }
+
+}
+
+public enum Status
+{
+    None = 0,
+    Aprovado = 1,
+    Reprovado = 2
+}
+
+public enum Disciplinas
+{
+    Historia,
+    Matematica,
+    Fisica,
+    Geografia
+}
